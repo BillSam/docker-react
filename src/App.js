bd5f25718bc0231f665ebc15e111056bdeb78055
mbugua <email@email.com>
2021-06-26 17:33:26 +0300

    new branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          I this is a deploy
+          This is a feature change
         </p>
       </div>
     );
